24a1285b6def599d2bb84728887b2809d9b62fc6
Cesar Ramirez <cesar_ramirez@hacienda.gov.py>
2026-03-25 16:48:15 -0300

    add observer in model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use App\Observers\PostObserver;
 
+#[ObservedBy(PostObserver::class)]
 class Post extends Model
 {
     use HasFactory;
