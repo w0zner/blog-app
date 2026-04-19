eb02e869f7983b40b7fa9897e83afe2cc769e6f9
Rodrigo Ramirez <cesarrodrigoramirez@gmail.com>
2026-04-19 01:48:33 -0300

    update test

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -3,7 +3,7 @@
 use App\Models\User;
 
 test('guests are redirected to the login page', function () {
-    $response = $this->get(route('dashboard'));
+    $response = $this->get(route('homr'));
     $response->assertRedirect(route('login'));
 });
 
@@ -11,6 +11,6 @@ test('authenticated users can visit the dashboard', function () {
     $user = User::factory()->create();
     $this->actingAs($user);
 
-    $response = $this->get(route('dashboard'));
+    $response = $this->get(route('home'));
     $response->assertOk();
 });
